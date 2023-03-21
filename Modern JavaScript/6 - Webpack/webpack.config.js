b5056b4cfbbf9fdd517492c7445ef2b742c58ed3
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-03-20 22:07:56 -0300

    Loaders in webpack

--- Modern JavaScript/6 - Webpack/webpack.config.js
+++ Modern JavaScript/6 - Webpack/webpack.config.js
@@ -1,13 +1,14 @@
-const path = require("path");
-
 module.exports = {
   entry: {
     index: "./src/index.js",
-    hello: "./src/hello.js",
   },
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    filename: "[name].bundle.min.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+    ],
   },
 };
