633564342caf96a52114971919a2c8cbd644e7d8
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-11 10:56:43 -0300

    POO em typescript

---
+++ 6 - TypeScript/12 - OO em Typescript/src/Spaceship.ts
@@ -0,0 +1,42 @@
+class Spaceship {
+  private _name: string;
+  protected captain: string;
+  protected speed: number;
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name: string) {
+    this._name = name;
+  }
+
+  constructor(name: string, captain: string) {
+    this._name = name;
+    this.captain = captain;
+    this.speed = 0;
+  }
+
+  public accelerate(rate: number, time: number) {
+    this.speed = rate * time;
+  }
+}
+
+class Fighter extends Spaceship {
+  weapons: number;
+
+  shoot() {
+    for (let i = 0; i < this.weapons; i++) {
+      console.log("Pew!");
+    }
+  }
+
+  erase() {
+    this.name = "";
+    this.captain = "";
+  }
+}
+
+let ship = new Spaceship("USS Enterprise", "James T. Kirk");
+
+ship.accelerate(50, 10);
