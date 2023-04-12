63f00edd4513ba7eef09d27ecbc661ce96b437f1
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-12 20:53:40 -0300

    React

---
+++ 7 - React/1 - Fundamentos React/2 - JSX/index.jsx
@@ -0,0 +1,14 @@
+const element = <h1>Olá, mundo!</h1>;
+
+const anotherElement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+      <li>Teste</li>
+    </ul>
+  </div>
+);
