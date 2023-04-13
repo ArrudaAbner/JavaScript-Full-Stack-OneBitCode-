2ce76a4c070c2b155acc85250624c26a54200df6
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-12 22:48:56 -0300

    Exercicio React

---
+++ 7 - React/1 - Fundamentos React/Exercicios/exercicio-obc/src/App.jsx
@@ -0,0 +1,39 @@
+export default function App() {
+  return (
+    <>
+      <img src="/react.png" alt="" />
+
+      <h1>React</h1>
+
+      <p>A blibioteca para interfaces de usuário web e nativas</p>
+      <button>Apernda React</button>
+      <button>Referência da API</button>
+      <hr />
+
+      <h1>Crie interfaces de usuário de components</h1>
+      <p>
+        React permite que você construa interfaces de usuário a partir de
+        pedaços individuais chamados componentes.
+      </p>
+      <hr />
+
+      <h1>Escreva componentes com código e marcação</h1>
+      <p>
+        Componentes React são funções JavaScript. A sintaxe de marcação é
+        chamada de JSX. É uma extensão da sintaxe JavaScript popularizada pelo
+        React.
+      </p>
+      <hr />
+
+      <h1>Próximos passos</h1>
+      <ul>
+        <li>Uso de dados dinâmicos no JSX</li>
+        <li>Criação de novos componentes</li>
+        <li>Estilização de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos do JavaScript</li>
+      </ul>
+    </>
+  );
+}
