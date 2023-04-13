62e2602a1bc17542cfb0e569157955c3499d0636
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-13 15:45:28 -0300

    Concentrando Rotas

--- 8 - Node JS/4 - To Do List/app.js
+++ 8 - Node JS/4 - To Do List/app.js
@@ -1,29 +1,10 @@
 const express = require("express");
+const checkListRouter = require("./src/routes/checklist");
 
 const app = express();
-
 app.use(express.json());
 
-const log = (req, res, next) => {
-  console.log(req.body);
-  console.log(`Data: ${Date.now()}`);
-  next();
-};
-
-app.use(log)
-
-app.get("/", (req, res) => {
-  res.send("<h1>Minha lista de tarefas </h1>");
-});
-
-app.get("/json", (req, res) => {
-  console.log(req.body);
-  res.json({ title: "Tarefa X", done: true });
-});
-
-app.get("/postman", (req, res) => {
-  res.send("<h1>Praticando get com postman</h1>");
-});
+app.use("/checklists", checkListRouter);
 
 app.listen(3000, () => {
   console.log("Servidor foi iniciado");
