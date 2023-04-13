d06d33bcd023b474e836392bcc536a5358a0f82c
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-12 21:55:44 -0300

    Componentes

---
+++ 7 - React/1 - Fundamentos React/3 - Components/index.jsx
@@ -0,0 +1,12 @@
+function PrimaryButton() {
+  return <button>Meu Botão</button>;
+}
+
+function Container() {
+  return (
+    <div className="" for>
+      <h1></h1>
+      <p></p>
+    </div>
+  );
+}
