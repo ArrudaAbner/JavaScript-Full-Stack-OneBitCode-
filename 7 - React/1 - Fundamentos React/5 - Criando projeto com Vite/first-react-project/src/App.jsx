8d84be9457b100cf59549db66c892f6622b5e865
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-12 22:36:01 -0300

    Criando projetos com Vite

---
+++ 7 - React/1 - Fundamentos React/5 - Criando projeto com Vite/first-react-project/src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return(
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Este é o meu primeiro projeto usando React e Vite</p>
+      <p>Próximos passos:</p>
+      <ul>
+        <li>Praticar com exercicios</li>
+      </ul>
+    </div>
+  )
+}
