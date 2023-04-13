b8b239107d2f8d31233071f3922a606919e32e7d
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-13 15:37:12 -0300

    Middleware

--- 8 - Node JS/4 - To Do List/app.js
+++ 8 - Node JS/4 - To Do List/app.js
@@ -2,19 +2,29 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const log = (req, res, next) => {
+  console.log(req.body);
+  console.log(`Data: ${Date.now()}`);
+  next();
+};
+
+app.use(log)
+
 app.get("/", (req, res) => {
   res.send("<h1>Minha lista de tarefas </h1>");
 });
 
 app.get("/json", (req, res) => {
-    res.json({title: "Tarefa X", done: true})
-})
+  console.log(req.body);
+  res.json({ title: "Tarefa X", done: true });
+});
 
 app.get("/postman", (req, res) => {
-    res.send("<h1>Praticando get com postman</h1>")
-})
+  res.send("<h1>Praticando get com postman</h1>");
+});
 
 app.listen(3000, () => {
   console.log("Servidor foi iniciado");
 });
-
