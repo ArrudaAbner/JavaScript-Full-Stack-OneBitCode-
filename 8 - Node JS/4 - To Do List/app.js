4c8acad2c72ad6665b1358cd10b17324afa197ca
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-13 15:23:25 -0300

    Postman

--- 8 - Node JS/4 - To Do List/app.js
+++ 8 - Node JS/4 - To Do List/app.js
@@ -10,6 +10,11 @@ app.get("/json", (req, res) => {
     res.json({title: "Tarefa X", done: true})
 })
 
+app.get("/postman", (req, res) => {
+    res.send("<h1>Praticando get com postman</h1>")
+})
+
 app.listen(3000, () => {
   console.log("Servidor foi iniciado");
 });
+
