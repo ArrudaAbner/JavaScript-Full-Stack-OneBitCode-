38302014ab638ef9ff527111c095b2e03ba33991
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-13 17:17:18 -0300

    Rotas POST

--- 8 - Node JS/4 - To Do List/src/routes/checklist.js
+++ 8 - Node JS/4 - To Do List/src/routes/checklist.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/Checklists", (req, res) => {
+router.get("/", (req, res) => {
   console.log("Olá");
   res.send();
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.status(200).json(req.body);
 });
 
 module.exports = router;
