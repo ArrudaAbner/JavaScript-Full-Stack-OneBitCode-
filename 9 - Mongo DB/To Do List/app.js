2cebeef969992856d3eee6af2461995fe493249e
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-04-14 10:51:59 -0300

    Mongo DB

---
+++ 9 - Mongo DB/To Do List/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const checkListRouter = require("./src/routes/checklist");
+
+const app = express();
+app.use(express.json());
+
+app.use("/checklists", checkListRouter);
+
+app.listen(3000, () => {
+  console.log("Servidor foi iniciado");
+});
